81516d02f776c0285664af997ca1f31c1c68bfcb
sivakrishnacode <sivakrishnacoc@gmail.com>
2023-06-16 12:56:05 +0530

    jun16

---
+++ index.js
@@ -0,0 +1,88 @@
+import dotenv from 'dotenv'
+import jwt from "jsonwebtoken";
+dotenv.config()
+
+
+
+  const TOKEN_SECRET = 'siva' ;
+  const REFRESH_SECRET = 'sivaa'
+
+  function signToken(prop={username :null, password: null , userDetails : null, isAuth: false}) {
+    return new Promise((resolve, reject) => {
+
+    if(!prop.username || prop.username == null) {
+      reject({
+        errCode:422,
+        errMessage : 'Username is Required'
+      })
+    }
+    if(!prop.password || prop.password == null) {
+      reject({
+        errCode:422,
+        errMessage : 'Password is Required'
+      })
+    }
+
+      jwt.sign(prop, TOKEN_SECRET, { expiresIn: '100' }, (error, token) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({'token': token , createdTime: Math.floor(Date.now() / 1000)});
+        }
+        
+      });
+    });
+  }
+
+
+
+
+
+  function checkToken(prop = { token: null}) {
+
+    return new Promise((resolve, reject) => {
+
+      if(!prop.token || prop.token == null){
+        reject({
+          errCode:422,
+          errMessage : 'Token is Required'
+        })
+      } 
+
+      jwt.verify(prop.token, TOKEN_SECRET, (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
+
+  function getTokenInfo(prop= {token: ''}){
+
+    return new Promise((resolve,reject ) => {
+    
+        const decoded = jwt.decode(prop.token);
+
+        if(!decoded){
+          reject(new Error('Invalid token'));
+         
+        }
+          else{
+            resolve(decoded);
+          }      
+    })
+  }
+
+
+  
+export default  () => {
+  return{
+     signToken,
+  checkToken,
+  getTokenInfo
+  }
+ 
+}
+ 
